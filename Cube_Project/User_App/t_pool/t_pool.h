a374c54d8951192c557adc0ecbf8b58ab315f6f6
Sephiroth <sephirothictreevii@gmail.com>
2019-08-23 17:25:20 +0800

    '\' 对齐微修

--- Cube_Project/User_App/t_pool/t_pool.h
+++ Cube_Project/User_App/t_pool/t_pool.h
@@ -9,34 +9,34 @@
 
 #define END_EXTERN_POOL(__NAME)
 /*============================ MACROFIED FUNCTIONS ===========================*/
-#define POOL_ITEM_INIT(__NAME, __POOL) \
+#define POOL_ITEM_INIT(__NAME, __POOL)                                       \
     __NAME##_pool_item_init((__POOL))
 
-#define POOL_ALLOCATE(__NAME, __POOL) \
+#define POOL_ALLOCATE(__NAME, __POOL)                                        \
     __NAME##_pool_allocate((__POOL))
 
-#define POOL_FREE(__NAME, __POOL, __ITEM) \
+#define POOL_FREE(__NAME, __POOL, __ITEM)                                    \
     __NAME##_pool_free((__POOL), (__ITEM))
 
-#define POOL_ADD_HEAP(_NAME, __POOL, __BUFFER, __SIZE) \
+#define POOL_ADD_HEAP(_NAME, __POOL, __BUFFER, __SIZE)                       \
     __NAME##_pool_add_heap((__POOL), (__BUFFER), (__SIZE))
 
 #define POOL(__NAME) __NAME##_item_pool_t
 
 #define PRINT_STR_POOL_ITEM_SIZE(__TYPE) (sizeof(__TYPE))
 
-#define EXTERN_POOL(__NAME, __TYPE)                                        \
-    typedef __NAME##_pool_item_t __NAME##_pool_item_t;                     \
-    union {                                                                \
-        uint_fast8_t ptBuffer[PRINT_STR_POOL_ITEM_SIZE[__TYPE]];           \
-        __NAME##_item_pool_t *ptNext;                                      \
-    } ALIGN(__alignof__(__TYPE));                                          \
-    extern void __NAME##_item_init(__NAME##_pool_item_t *ptPool);          \
-    extern _TYPE *__NAME##_pool_allocate(__NAME##_pool_item_t *ptPool);    \
-    extern bool __NAME##_pool_add_heap(__NAME##_pool_item_t *ptPool,       \
-                                       uint8_t *pTarget, uint16_t hwSize); \
-    extern void __NAME##_pool_free(__NAME##_pool_item_t *ptPool,           \
-                                   __TYPE *ptItem);                        \
+#define EXTERN_POOL(__NAME, __TYPE)                                          \
+    typedef __NAME##_pool_item_t __NAME##_pool_item_t;                       \
+    union {                                                                  \
+        uint_fast8_t ptBuffer[PRINT_STR_POOL_ITEM_SIZE[__TYPE]];             \
+        __NAME##_item_pool_t *ptNext;                                        \
+    } ALIGN(__alignof__(__TYPE));                                            \
+    extern void __NAME##_item_init(__NAME##_pool_item_t *ptPool);            \
+    extern _TYPE *__NAME##_pool_allocate(__NAME##_pool_item_t *ptPool);      \
+    extern bool __NAME##_pool_add_heap(__NAME##_pool_item_t *ptPool,         \
+                                       uint8_t *pTarget, uint16_t hwSize);   \
+    extern void __NAME##_pool_free(__NAME##_pool_item_t *ptPool,             \
+                                   __TYPE *ptItem);                          \
     static void __NAME##_pool_push(__NAME##_pool_item_t *ptPool);
 
 #define DEF_POOL_EX(__NAME, __TYPE)                                          \
