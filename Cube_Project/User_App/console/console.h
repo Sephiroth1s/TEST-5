0bb9d110b0320691e89f0af01f4a1093a9be5808
Sephiroth <sephirothictreevii@gmail.com>
2019-10-12 18:07:55 +0800

    待修改

--- Cube_Project/User_App/console/console.h
+++ Cube_Project/User_App/console/console.h
@@ -12,6 +12,16 @@ struct processing_string_evt_handler_t {
     void *pTarget;
 };
 
+typedef fsm_rt_t special_key_t();
+typedef struct special_key_evt_handler_t special_key_evt_handler_t;
+struct special_key_evt_handler_t {
+    uint8_t chState;
+    event_t *ptRepeatByte;
+    event_t *ptRepeatLine;
+    uint8_t chLastCounter;
+    uint8_t chLastMaxNumber;
+};
+
 typedef struct {
     uint8_t chState;
     read_byte_evt_handler_t *ptReadByteEvent;
@@ -19,14 +29,11 @@ typedef struct {
     void *pOutputTarget;
     print_str_t *ptPrintStr;
     uint8_t *pchCurrentBuffer;
-    uint8_t *pchLastBuffer;
     uint8_t chByte;
     uint8_t chCurrentCounter;
-    uint8_t chLastCounter;
     uint8_t chMaxNumber;
-    uint8_t chLastMaxNumber;
-    event_t *ptRepeatByte;
-    event_t *ptRepeatLine;
+    uint8_t *pchLastBuffer;
+    special_key_evt_handler_t *ptSpecialKey;
 } console_print_t;
 
 typedef struct {
@@ -36,11 +43,11 @@ typedef struct {
     uint8_t *pchCurrentBuffer;
     uint8_t *pchLastBuffer;
     void *pOutputTarget;
-    event_t *ptRepeatByte;
-    event_t *ptRepeatLine;
+    special_key_evt_handler_t *ptSpecialKey;
 } console_print_cfg_t;
 
 extern bool task_console_init(console_print_t *ptThis, console_print_cfg_t *ptCFG);
 extern fsm_rt_t task_console(console_print_t *ptThis);
+static fsm_rt_t special_key_evt_handler(special_key_evt_handler_t *ptTarget,uint8_t *pchBuffer);
 
 #endif
